3179e3d058313c0965eee2bfcee32a91459950a2
Surya Sonar <iamsuryasonar@gmail.com>
2024-01-02 23:17:30 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -69,9 +69,8 @@ function App() {
             return (
               <div key={item.id} className='relative w-full h-full grid mb-4 rounded-lg'>
                 <img className='' src={item.image} loading='lazy'></img>
-                {/*top-0 left-0 right-0 bottom-0 flex items-center justify-center  opacity-0 hover:opacity-65 transition-opacity duration-300 */}
-                <div className='absolute top-0 left-0 right-0 bottom-0 grid bg-black opacity-0 hover:opacity-65 transition-opacity duration-300'>
-                  <div className='grid p-6 place-self-center'>
+                <div className='absolute top-0 left-0 right-0 bottom-0 grid place-content-center bg-black opacity-0 hover:opacity-65 transition-opacity duration-300'>
+                  <div className='grid p-6 '>
                     <p className='place-self-end text-white font-normal font-base' >{item.quote}</p>
                     <p className='place-self-end text-white font-light text-sm'> {item.location}</p>
                   </div>
