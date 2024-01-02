f812339785ac8ee92c12f7bb3260ce46f49dbc5d
Surya Sonar <iamsuryasonar@gmail.com>
2024-01-02 22:29:46 +0530

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -67,9 +67,9 @@ function App() {
         <div className=" columns-1 md:columns-2 px-6 gap-4">
           {gallery.map((item, index) => {
             return (
-              <div key={item.id} className='relative group w-full h-full  grid mb-4 rounded-lg'>
+              <div key={item.id} className='relative w-full h-full grid mb-4 rounded-lg'>
                 <img className='' src={item.image} loading='lazy'></img>
-                <div className='absolute flex items-center justify-center  w-full h-full top-0 left-0 botton-0 right-0 bg-black opacity-0 group-hover:opacity-65 transition-opacity duration-300'>
+                <div className='absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center opacity-0 hover:opacity-65 transition-opacity duration-300'>
                   <div className='grid p-6'>
                     <p className='place-self-end text-white font-normal font-base' >{item.quote}</p>
                     <p className='place-self-end text-white font-light text-sm'> {item.location}</p>
