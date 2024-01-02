d148f8c0679b8bb6d3513ca273bd6b9278a620b3
Surya Sonar <iamsuryasonar@gmail.com>
2024-01-03 01:20:17 +0530

    data edited

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,43 @@ import './App.css'
 function App() {
 
   const gallery = [
-
     {
-      id: "caterpillar",
-      quote: "A tiny warrior with Spider-Man eyes",
-      location: "Assam, India",
+      id: "1",
+      quote: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+      location: "India",
       image: 'https://source.unsplash.com/random/6',
     },
     {
-      id: "humanEvolution",
-      quote: "Four figures stepping out of the water, a symbolic depiction of human evolution and the eternal cycle of rebirth",
-      location: "Assam, India",
+      id: "2",
+      quote: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+      location: "India",
       image: 'https://source.unsplash.com/random/1',
     },
 
 
     {
-      id: "roadside_rythm",
+      id: "3",
       quote: "Witness the dance of trees as seen through the lens of a passing car.",
-      location: "Assam, India",
+      location: "India",
       image: 'https://source.unsplash.com/random/2',
     },
 
     {
-      id: "forever",
-      quote: "Lost in thought, a couple peers into the distance, where the unseen horizon becomes a canvas for their shared aspirations.",
-      location: "Assam, India",
+      id: "4",
+      quote: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+      location: "India",
       image: 'https://source.unsplash.com/random/3',
     },
     {
-      id: "beautifulEyes",
-      quote: "Through a charming pose, a child reveals a world of innocence.",
-      location: "Assam, India",
+      id: "5",
+      quote: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+      location: "India",
       image: 'https://source.unsplash.com/random/4',
     },
     {
-      id: "glitteringSand",
-      quote: "A silhouette on the sands, where glittering grains meet the silhouette form of a boy, under the watchful gaze of a distant mountain.",
-      location: "Assam, India",
+      id: "6",
+      quote: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.",
+      location: "India",
       image: 'https://source.unsplash.com/random/5',
     },
   ]
@@ -50,8 +49,8 @@ function App() {
 
   return (
     <>
-      <button onClick={() => setValue(!value)} className='p-6 bg-black text-white'>click me</button>
-      {value && <div className='bg-green-200 w-full h-10 p-6'>button clicked</div>}
+      <button onClick={() => setValue(!value)} className='p-6 bg-black text-white'>TAP TAP</button>
+      {value && <div className='bg-green-200 w-full h-10 p-6'>I'm visible!</div>}
       <div className='w-screen my-20 grid place-items-center'>
         <div className=" columns-1 md:columns-2 px-6 gap-4">
           {gallery.map((item, index) => {
@@ -60,6 +59,7 @@ function App() {
                 <img className='' src={item.image} loading='lazy'></img>
                 {/* replace below 'flex justify-center items-center' to -> 'grid place-content-center' that will fix the desktop mode browser tab hang issue */}
                 <div className='grid place-content-center absolute top-0 left-0 right-0 bottom-0  bg-black opacity-0 hover:opacity-65 transition-opacity duration-300'>
+                  {/* this div needs to be centered */}
                   <div className='grid p-6 '>
                     <p className='place-self-end text-white font-normal font-base' >{item.quote}</p>
                     <p className='place-self-end text-white font-light text-sm'> {item.location}</p>
