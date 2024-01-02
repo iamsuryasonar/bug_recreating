7e2963b4225f3d73476fcba24b0552afdf1b038d
Surya Sonar <iamsuryasonar@gmail.com>
2024-01-02 22:14:49 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -67,17 +67,18 @@ function App() {
         <div className=" columns-1 md:columns-2 px-6 gap-4">
           {gallery.map((item, index) => {
             return (
-              <div key={item.id} className='relative group w-full h-full  grid mb-4 rounded-lg'>
-                <img className={`object-cover`} src={item.image} loading='lazy'></img>
-                <div className='absolute flex items-center justify-center  w-full h-full top-0 left-0 botton-0 right-0 bg-black opacity-0 group-hover:opacity-65 transition-opacity duration-300'>
+              // group
+              <div key={item.id} className='relative  w-full h-full  grid mb-4 rounded-lg'>
+                <img className='' src={item.image} loading='lazy'></img>
+                {/* <div className='absolute flex items-center justify-center  w-full h-full top-0 left-0 botton-0 right-0 bg-black opacity-0 group-hover:opacity-65 transition-opacity duration-300'>
                   <div className='grid p-6'>
                     <p className='place-self-end text-white font-normal font-base' >{item.quote}</p>
                     <p className='place-self-end text-white font-light text-sm'> {item.location}</p>
                   </div>
-                </div>
+                </div> */}
               </div>)
           })}
-          <div className=" max-w-4xl columns-1 md:columns-2 px-6 gap-4">
+          {/* <div className=" max-w-4xl columns-1 md:columns-2 px-6 gap-4">
             <img class="mb-4" src="https://source.unsplash.com/random/1" />
             <img class="mb-4" src="https://source.unsplash.com/random/2" />
             <img class="mb-4" src="https://source.unsplash.com/random/3" />
@@ -85,7 +86,7 @@ function App() {
             <img class="mb-4" src="https://source.unsplash.com/random/5" />
             <img class="mb-4" src="https://source.unsplash.com/random/6" />
             <img class="mb-4" src="https://source.unsplash.com/random/7" />
-          </div>
+          </div> */}
 
 
           {/* </span>
